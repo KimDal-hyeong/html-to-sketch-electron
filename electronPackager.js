69edbb9b1622115ea321a9f06af61909bc1cd22d
kimdalhyeong <5gockcook@naver.com>
2018-01-21 06:36:02 +0900

    패키징 작업 스크립트로 정리, 커맨드 수정

---
+++ electronPackager.js
@@ -0,0 +1,25 @@
+const packager = require('electron-packager');
+
+const options = {
+  dir: '.',
+  out: 'app-build',
+  name: 'HTML to Sketch',
+  icon: 'resources/icon.icns',
+  platform: 'darwin',
+  arch: 'x64',
+  overwrite: true,
+  prune: true,
+  ignore: [
+    '.gitignore',
+    'electronPackager.js',
+    'gulpfile.js',
+    'package-lock.json',
+    'webpack.config.js',
+    'yarn.lock',
+  ]
+};
+
+packager(options)
+  .then((appPaths) => {
+    console.log('Build Complete : ' + appPaths)
+  });
