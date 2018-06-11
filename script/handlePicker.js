1acc7f597eabbb921b4a33d15e1ba42186e0c24b
kimdalhyeong <5gockcook@naver.com>
2018-06-12 03:05:01 +0900

    fix picker cancel btn

--- script/handlePicker.js
+++ script/handlePicker.js
@@ -2,17 +2,17 @@ const {webview, pickerBtn, runBtn} = require(__dirname + '/getElements');
 
 function startPicker() {
   webview.executeJavaScript('webviewScript.startPicker();');
-  pickerBtn.setAttribute('onclick', 'stopPicker();');
+  pickerBtn.removeEventListener('click', startPicker);
+  pickerBtn.addEventListener('click', stopPicker);
   pickerBtn.classList.add('SideBar__pick-btn--stop-mode');
 }
 
 function stopPicker() {
   webview.executeJavaScript('webviewScript.stopPicker();');
-  pickerBtn.setAttribute('onclick', 'startPicker();');
+  pickerBtn.removeEventListener('click', stopPicker);
+  pickerBtn.addEventListener('click', startPicker);
   runBtn.classList.remove('SideBar__run-btn--picked-mode') ;
   pickerBtn.classList.remove('SideBar__pick-btn--stop-mode');
 }
-
-pickerBtn.addEventListener('click', startPicker);
 
 module.exports = {startPicker, stopPicker};
