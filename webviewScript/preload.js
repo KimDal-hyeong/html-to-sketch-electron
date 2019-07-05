3ee7fc4eb2c2dd26848df7135c805a6030066114
Dennis <dh.kim@modusign.co.kr>
2019-07-06 03:11:16 +0900

    불필요한 open 모듈 제거

--- webviewScript/preload.js
+++ webviewScript/preload.js
@@ -1,9 +1,7 @@
 const {ipcRenderer} = require("electron");
-const openUrlInBrowser = require("open");
 
 window.htmlToSketch = {
   sendToHost(channel, ...args) {
     ipcRenderer.sendToHost(channel, args)
   },
-  openUrlInBrowser,
 };
