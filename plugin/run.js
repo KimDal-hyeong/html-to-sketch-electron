fa3102637e98f9884b10f426ca64c5e026972461
Dennis <dh.kim@modusign.co.kr>
2019-07-06 00:59:19 +0900

    플러그인 작동 방식 변경

--- plugin/run.js
+++ plugin/run.js
@@ -1,6 +1,8 @@
 import asketch2sketch from '@brainly/html-sketchapp/asketch2sketch/asketch2sketch';
 
 export default function run(context) {
-  const asketchFile = /*JSON*/{}/*JSON*/;
-  asketch2sketch(context, [asketchFile]);
+  const url = NSURL.URLWithString('file://' + context.jsonPath.replace(' ', '%20'));
+  const data = NSData.dataWithContentsOfURL(url);
+  const content = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding);
+  asketch2sketch(context, [JSON.parse(content)]);
 }
--- script/runPlugin.js
+++ script/runPlugin.js
@@ -3,25 +3,18 @@ const path = require('path');
 const exec = require('child_process').exec;
 const { sketchtoolRunCommand } = require('@skpm/builder/lib/utils/webpackCommandPlugin/webpackShellPlugin');
 const { app } = require('electron').remote;
-const pluginDirPath = path.resolve(app.getPath('appData'), 'html-to-sektch-electron');
 
-const JSON_POSITION_COMMENT = '/*JSON*/';
+const jsonPath = path.resolve(app.getPath('appData'), 'html-to-sektch-electron', 'json.json');
 
 async function runPlugin (json, callback) {
-  // appData에 플러그인이 있는지 확인하고 없으면 복사
-  if (!fs.existsSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin'))) {
-    fs.mkdirSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin'));
-    fs.mkdirSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents'));
-    fs.mkdirSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents/Sketch'));
-    fs.copyFileSync(path.resolve(__dirname, '../plugin/asketch2sketch.sketchplugin/Contents/Sketch/run.js'), path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents/Sketch/run.js'));
-    fs.copyFileSync(path.resolve(__dirname, '../plugin/asketch2sketch.sketchplugin/Contents/Sketch/manifest.json'), path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents/Sketch/manifest.json'));
-  }
+  fs.writeFileSync(jsonPath, json);
 
-  const pluginCode = fs.readFileSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents/Sketch/run.js'), 'utf8');
-  const newPluginCode = pluginCode.split(JSON_POSITION_COMMENT).map((code, index) => (index === 1 ? json : code)).join(JSON_POSITION_COMMENT);
-  fs.writeFileSync(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin/Contents/Sketch/run.js'), newPluginCode);
+  const command = sketchtoolRunCommand(
+    path.resolve(__dirname, '../plugin', 'asketch2sketch.sketchplugin'),
+    'run',
+    { context: { jsonPath } }
+  );
 
-  const command = sketchtoolRunCommand(path.resolve(pluginDirPath, 'asketch2sketch.sketchplugin'), 'run');
   exec(command, (error, stdout) => {
     if (error) {
       console.error(`exec error: ${error}`);
