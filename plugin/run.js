26fadac6ab282bf764b7784e6f58477e8d694cc2
KimDal-hyeong <5gockcook@naver.com>
2020-05-24 05:45:37 +0900

    기존의 레이어를 삭제하지 않도록 변경

--- plugin/run.js
+++ plugin/run.js
@@ -4,5 +4,5 @@ export default function run(context) {
   const url = NSURL.URLWithString('file://' + context.jsonPath.replace(' ', '%20'));
   const data = NSData.dataWithContentsOfURL(url);
   const content = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding);
-  asketch2sketch(context, [JSON.parse(content)]);
+  asketch2sketch(context, [JSON.parse(content)], { clearPage: false });
 }
